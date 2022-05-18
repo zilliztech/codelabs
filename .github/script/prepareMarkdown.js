904392506dbdabb50a8e231d4a4c2a24edc5bb44
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2022-05-18 22:48:48 +0800

    format markdown for pdf ready

---
+++ .github/script/prepareMarkdown.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const glob = require('glob');
+
+const CODELABS_DIR = 'codelabs';
+// get all codelab.json from claat generated files
+const mdFiles = glob.sync(`${CODELABS_DIR}/*/index.md`);
+
+for (let i = 0; i < mdFiles.length; i++) {
+  const mdFile = mdFiles[i];
+  const mdFileContents = fs.readFileSync(mdFile, 'utf-8');
+  const newFile = mdFile.replace(`index.md`, `pdf.md`);
+
+  const newMarkdownContent = [];
+  const frontmatter = [];
+  const lines = mdFileContents.split(/\r?\n/);
+
+  let frontmatter_set = false;
+
+  lines.forEach((line, index) => {
+    // console.log(`Line from file: ${line}`);
+
+    if (frontmatter_set) {
+      if (!line.startsWith('Duration:')) {
+        newMarkdownContent.push(line);
+      }
+    } else {
+      frontmatter.push(line);
+      const nextLine = lines[index + 1];
+      frontmatter_set = nextLine.startsWith('# ') || nextLine === '';
+      console.log('frontmatter_set', mdFileContents);
+    }
+  });
+
+  console.log(frontmatter);
+
+  // save article to disk
+  fs.writeFile(
+    newFile,
+    ['---', ...frontmatter, '---', ...newMarkdownContent].join('\n'),
+    err => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+}

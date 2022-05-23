ab5debeba30b03e0fc6ca7c68c637fb974ee3e89
ThyeeZz <jialian.ji@zilliz.com>
2022-05-23 15:09:21 +0800

    complete tohi header and footer

--- src/commonComponents/towhee/footer/index.jsx
+++ src/commonComponents/towhee/footer/index.jsx
@@ -1,100 +1,102 @@
-import React from "react";
-import { RectAngle, Github, Slack, Twitter } from "./icons";
-import logo from "../images/logo-title.png";
-import classes from "./index.module.less";
+import React from 'react';
+import { RectAngle, Github, Slack, Twitter } from './icons';
+import logo from '../images/logo-title.png';
+import classes from './index.module.less';
 
 export default function TowheeFooter() {
   return (
     <footer className={classes.footerWrapper}>
-      <div className={classes.topContent}>
-        <a href="https://towhee.io" className="logo-wrapper">
-          <img src={logo} alt="logo" />
-        </a>
+      <div className={classes.footerContent}>
+        <div className={classes.topContent}>
+          <a href="https://towhee.io" className={classes.logoWrapper}>
+            <img src={logo} alt="logo" />
+          </a>
 
-        <nav>
-          <ul>
+          <nav>
+            <ul>
+              <li>
+                <a href="https://github.com/towhee-io/towhee" target="_blank">
+                  Contribute
+                </a>
+              </li>
+              <li>
+                <a href="https://towhee.io/pipelines">Pipelines</a>
+              </li>
+              <li>
+                <a href="https://towhee.io/operators">Operators</a>
+              </li>
+              <li>
+                <a href="https://docs.towhee.io/" target="_blank">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="https://demos.towhee.io/" target="_blank">
+                  Demo
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <ul className={classes.socials}>
+            <li>
+              <a href="https://slack.towhee.io" target="_blank">
+                <Slack />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/towheeio" target="_blank">
+                <Twitter />
+              </a>
+            </li>
             <li>
               <a href="https://github.com/towhee-io/towhee" target="_blank">
-                Contribute
-              </a>
-            </li>
-            <li>
-              <a href="https://towhee.io/pipelines">Pipelines</a>
-            </li>
-            <li>
-              <a href="https://towhee.io/operators">Operators</a>
-            </li>
-            <li>
-              <a href="https://docs.towhee.io/" target="_blank">
-                Documentation
-              </a>
-            </li>
-            <li>
-              <a href="https://demos.towhee.io/" target="_blank">
-                Demo
+                <Github />
               </a>
             </li>
           </ul>
-        </nav>
+        </div>
 
-        <ul className="socials">
-          <li>
-            <a href="https://slack.towhee.io" target="_blank">
-              <Slack />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/towheeio" target="_blank">
-              <Twitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/towhee-io/towhee" target="_blank">
-              <Github />
-            </a>
-          </li>
-        </ul>
-      </div>
+        <div className={classes.btmContent}>
+          <nav className={classes.footNav}>
+            <ul>
+              <li>
+                <a href="https://github.com/towhee-io/towhee" target="_blank">
+                  Contribute
+                </a>
+              </li>
+              <li>
+                <a href="https://towhee.io/pipelines">Pipelines</a>
+              </li>
+              <li>
+                <a href="https://towhee.io/operators">Operators</a>
+              </li>
+              <li>
+                <a href="https://docs.towhee.io/" target="_blank">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="https://demos.towhee.io/" target="_blank">
+                  Demo
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-      <div className={classes.btmContent}>
-        <nav className="foot-nav">
-          <ul>
-            <li>
-              <a href="https://github.com/towhee-io/towhee" target="_blank">
-                Contribute
-              </a>
-            </li>
-            <li>
-              <a href="https://towhee.io/pipelines">Pipelines</a>
-            </li>
-            <li>
-              <a href="https://towhee.io/operators">Operators</a>
-            </li>
-            <li>
-              <a href="https://docs.towhee.io/" target="_blank">
-                Documentation
-              </a>
-            </li>
-            <li>
-              <a href="https://demos.towhee.io/" target="_blank">
-                Demo
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="copyright-section">
-          <div className="blocks-wrapper">
-            <span className="rectangle-1">
-              <RectAngle />
-            </span>
-            <span className="rectangle-2">
-              <RectAngle />
-            </span>
+          <div className={classes.copyrightSection}>
+            <div className={classes.blocksWrapper}>
+              <span>
+                <RectAngle />
+              </span>
+              <span className={classes.rectangl}>
+                <RectAngle />
+              </span>
+            </div>
+            <p className={classes.copyright}>
+              © {new Date().getFullYear()} Towhee. All rights reserved.
+            </p>
           </div>
-          <p className="copyright">
-            © {new Date().getFullYear()} Towhee. All rights reserved.
-          </p>
         </div>
       </div>
     </footer>
